0572b8f1dfdbd5621ec9ae5b4bfd2584f407d661
Edson <alves.ed1988@gmail.com>
2020-07-19 00:23:07 -0300

    criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=32):
+    def __init__(self, *filhos, nome=None, idade=32):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Eddy')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Edson'
-    print(p.nome)
-    print(p.idade)
+    edson = Pessoa(nome='Edson')
+    eddy = Pessoa(edson, nome='Eddy')
+    print(Pessoa.cumprimentar(eddy))
+    print(id(eddy))
+    print(eddy.cumprimentar())
+    print(eddy.nome)
+    print(eddy.idade)
+    for filho in eddy.filhos:
+        print(filho.nome)
